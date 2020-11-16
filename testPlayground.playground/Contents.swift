653d59dfeaa1aab2f3813c5d8bb9b2503da8ebfa
Serhio Brit <serhiobrit@MacBook-Pro.local>
2020-11-16 23:20:56 +0300

    module 16 test code

--- testPlayground.playground/Contents.swift
+++ testPlayground.playground/Contents.swift
@@ -1,80 +1,122 @@
 import UIKit
+import Foundation
 
-//Все же знают автомобиль Тесла? Как вы, наверное, знаете, у него есть режим автопилота. Но если в системе происходит какой-либо сбой или поломка, машина сообщает об этом и просит перейти в ручной режим управления. Сегодня нам предстоит написать аналог системы, которая будет сообщать об ошибке в системе автопилота.
+// ---------------------- 1 -----------------------
+print ("---------------------- 1 -----------------------")
+//Создайте кортеж для двух человек с одинаковыми типами данных и параметрами.
+//При том одни значения доставайте по индексу, а другие — по параметру.
 
-//Переменная, которая хранит в себе режим управления: ручной или автопилот. Если включен режим ручного управления, то будем считать, что автопилот отключен.
+let firstPerson = (age: 18, name: "Ivan", surname: "Ivanov")
+let secondPerson = (age: 24, name: "Petr", surname: "Petrov")
+let firstPersonAge = firstPerson.0
+let firstPersonName = firstPerson.1
+let firstPersonLastName = firstPerson.2
+let sesondPersonAge = secondPerson.age
+let sesondPersonName = secondPerson.name
+let sesondPersonLastName = secondPerson.surname
 
-//Должно быть реализовано хранение каких-либо характеристик автомобиля, например:
-//Масса: 2108 кг
-//Длина: 4976 мм
-//Ширина (с учетом боковых зеркал): 1963 мм
-//Высота: 1435 мм
-//Колесная база: 2959 мм
-//Клиренс: 154.9 мм
-//Объем багажника: 900 литров
-
-//Должно быть реализовано хранение функционала машины. Обязательный функционал: управление музыкой, люком (степень его открытия измеряется в процентах), скорость вентилятора обдува, климатические установки для водительского места. На своё усмотрение вы можете добавить что-то ещё. Проявите фантазию!
-
-//Должна быть реализована система обработки ошибок, которые могут произойти. Если сработала ошибка, то нужно вывести соответствующее сообщение в консоль, указать, какая именно ошибка, и как её можно решить. Например: потеря на дороге → нужно включить GPS, низкий заряд батареи → нужно сообщить об этом и отобразить ближайшую станцию подзарядки. Проявите фантазию!
-
-//Параметров может быть неограниченное количество, но их нужно обрабатывать и выводить соответствующие сообщения в консоль. Желательно использовать do-catch, хотя и необязательно, т.к. данная тема в полном объёме не была пройдена, однако какие-то базовые вещи в данном модуле были даны.
-
-//Если пользователь включает какую-либо функцию, например, тот же автопилот, должно выводиться сообщение о том, что это может быть опасно для водителя, и нужно следить за движением автомобиля.
+// ---------------------- 2 -----------------------
+print ("---------------------- 2 -----------------------")
+//Создайте массив «дни в месяцах» (12 элементов содержащих количество дней в соответствующем месяце). Используя цикл for и этот массив:
+//выведите количество дней в каждом месяце
+//используйте еще один массив с именами месяцев чтобы вывести название месяца + количество дней
 
 
-// характеристики автомобиля
-let mass: Double = 2108 // масса в килограммах
-let length: Double = 4976 // длина в миллиметрах
-let width: Double = 1963 //  ширина (с учетом боковых зеркал) в миллиметрах
-let height:Double = 1435 // высота в миллиметрах
-let wheelBase:Double = 2959 // колесная база в миллиметрах
-let cleerance: Double = 154.9 // клиренс в миллиметрах
-let trunkVolume = 900.0 // объем багажника в литрах
-var percentage = 100
+let months: [String] = ["January", "Fabuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
-// функционал машины
-var isGpsOn: Bool = false // включение GPS
-var isAutoPilotOn: Bool = true // режим автопилота, по-умолчанию, влючен; если выключен, то включен режим ручного управления
-var energySafe: Bool = false // режим энергосбережения
+let daysInMonth: [Int] = [31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 31]
 
-// перечень ошибок
-enum AutoError: Error {
-    case isLost // заблудился
-    case lowBattery // низкая батарея
-    case brokeAutoDrive // проблемы с управлением машины
+for month in 1..<months.count {
+    print ("В \(months[month]) \(daysInMonth[month]) дней")
 }
 
-// задание ситуации
-var isLost: Bool = false
-// var lowBattery: Bool = false
-var brokeAutoDrive: Bool = true
+print ("------------------------------------------------------------------------------")
 
-// процент зарядки
+//сделайте тоже самое, но используя массив кортежей с параметрами (имя месяца, количество дней)
+
+var january: (String, Int) = ("January", 31)
+var fabuary: (String, Int) = ("Fabuary", 30)
+var march: (String, Int) = ("March", 31)
+var april: (String, Int) = ("April", 30)
+var may: (String, Int) = ("May", 31)
+var june: (String, Int) = ("June", 30)
+var july: (String, Int) = ("July", 31)
+var august: (String, Int) = ("August", 30)
+var september: (String, Int) = ("September", 31)
+var october: (String, Int) = ("October", 30)
+var november: (String, Int) = ("November", 31)
+var december: (String, Int) = ("December", 31)
+
+var daysInMonth1 = [january, fabuary, march, april, may, june, july, august, september, october, november, december]
+
+for month in 0..<daysInMonth1.count {
+    print ("В \(daysInMonth1[month].0) \(daysInMonth1[month].1) дней")
+}
+
+print ("------------------------------------------------------------------------------")
+
+//сделайте тоже самое, только выводите дни в обратном порядке (порядок в исходном массиве не меняется)
+
+for month in (0..<daysInMonth1.count).reversed() {
+    print ("В \(daysInMonth1[month].0) \(daysInMonth1[month].1) дней")
+}
+
+print ("------------------------------------------------------------------------------")
 
 
-// обработка ошибок
-func autoDrive() throws {
-    if isLost {
-        isGpsOn = true
-        throw AutoError.isLost
+//для произвольно выбранной даты (месяц и день) посчитайте количество дней до конца года
+//я придумал пока только вариант с заданием и номера месяца, но не понял, как его высчитать можно
+
+let today: (String, Int, Int) = ("November", 16, 10)
+var daysTilMonthEnd: Int
+
+func summDays (chooseMonth: Int) -> Int {
+    let leftMonths = daysInMonth1.filter{$0 > daysInMonth1[chooseMonth]}
+    var leftDays: Int = 0
+    for i in 0..<leftMonths.count {
+        leftDays += leftMonths[i].1
     }
- 
-    if lowBattery {
-        throw AutoError.lowBattery
-    }
- 
-    if brokeAutoDrive {
-        isAutoPilotOn = false
-        throw AutoError.brokeAutoDrive
+    return leftDays
+}
+
+for month in 0..<daysInMonth1.count {
+    if today.0 == daysInMonth1[month].0 {
+        daysTilMonthEnd = daysInMonth1[month].1 - today.1
+        let summDaysTilYearEnd = daysTilMonthEnd + summDays(chooseMonth: today.2)
+        print ("До конца года осталось \(summDaysTilYearEnd) дней")
     }
 }
- 
-do {
-    try autoDrive()
-} catch AutoError.isLost {
-    print("Вы заблудились! Включаю GPS")
-} catch AutoError.lowBattery {
-    print("Батарея садится! Ближайшая станция подзарядки через 1 км 300 м")
-} catch AutoError.brokeAutoDrive {
-    print("Режим автопилота поврежден. Переходим в режим ручного управления!")
+
+// ---------------------- 3 -----------------------
+print ("---------------------- 3 -----------------------")
+//Создайте словарь, как журнал студентов, где имя и фамилия студента это ключ, а оценка за экзамен — значение.
+
+var students: [String: Int] = ["Иванов Иван": 5, "Петров Петр": 1, "Семенов Семен": 3]
+
+//Повысьте студенту оценку за экзамен
+//Если оценка положительная (4 или 5) или удовлетворительная (3), то выведите сообщение с поздравлением, отрицательная (1, 2) - отправьте на пересдачу
+
+let newMark = 4
+let studentName = "Семенов Семен"
+students.updateValue(newMark, forKey: studentName)
+if students[studentName]! > 2 {
+    print("Поздравляю с успешной пересдачей")
+} else {
+    print("Стоит подготовиться получше и попробовать в другой раз")
 }
+
+//Добавьте еще несколько студентов — это ваши новые одногруппники!
+
+students["Новая Галина"] = 3
+students["Новая Светлана"] = 5
+
+//Удалите одного студента — он отчислен
+
+students["Петров Петр"] = nil
+
+//Посчитайте средний балл всей группы по итогам экзамена.
+//Не пойму, как в Double перевести -- он сначала считает в Int, а потом уже переводит в Double
+
+let marks = [Int](students.values)
+let total = marks.reduce(0, +)
+let bandScrore = Double (total / marks.count)
